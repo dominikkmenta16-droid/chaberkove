975f04ade5fce2179397783cc95653c19aa6e0d8
dominikkmenta16-droid <dominik.kmenta16@gmail.com>
2025-11-22 15:47:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let cart = [];
+let total = 0;
+
+function addToCart(name, price) {
+    cart.push({name, price});
+    total += price;
+    renderCart();
+}
+
+function renderCart() {
+    const cartList = document.getElementById('cart');
+    cartList.innerHTML = '';
+    cart.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.name} - ${item.price} Kè`;
+        cartList.appendChild(li);
+    });
+    document.getElementById('total').textContent = total;
+}
